77d39b147fdbf4fb8466db724fc335b0144fb2d0
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2025-11-09 11:42:56 +0300

    add styles 2 to todo

--- src/js/index.js
+++ src/js/index.js
@@ -19,10 +19,11 @@
 
 // window.localStorage.setItem('cart', jsonCart);
 
+
 import $ from 'jquery';
 import { v4 as uuidv4 } from 'uuid';
+import Task from './task.js';
 import '../scss/index.scss';
-import Task from './task';
 
 let tasks = JSON.parse(window.localStorage.getItem('tasks'));
 
@@ -30,70 +31,68 @@ if(!tasks) {
   tasks = [];
 }
 
-console.log(tasks);
-
-function renderList() {
+function addTaskToList(task) {
   const list = $('.tasks');
-  list.html(null);
-  tasks.forEach((el, i) => {
-    addTaskToList(el, i);
-  });
-}
+  const li = $(`<li class="${task.status}">${task.name}</li>`);
+  const buttons = $('<div class="btns"></div>')
+  const doneButton = $('<button class="btn do">&#10004;</button>');
+  const removeButton = $('<button class="btn rem">&#10006;</button>');
 
-function addTaskToList(task, i) {
-  const list = $('.tasks');
-  const li = $(`<li class="${task.status}">${i + 1}. ${task.name}</li>`);
-  const doneButton = $('<button>Do!</button>');
   doneButton.click(() => {
     tasks.forEach((item, index, tasks) => {
       if(item.id == task.id) {
         tasks[index].status = 'done';
       }
     });
+
+  window.localStorage.setItem('tasks', JSON.stringify(tasks));
     renderList();
-    window.localStorage.setItem('tasks', JSON.stringify(tasks));
   });
-  const removeButton = $('<button>Remove</button>');
+
   removeButton.click(() => {
-    if(confirm('???')) {
-      tasks.forEach((item, index, tasks) => {
-        if(item.id == task.id) {
-          tasks.splice(index, 1);
-        }
-      });
-      renderList();
-      window.localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-  });
+  if(confirm('Delete?')){
+    tasks.forEach((item, index, tasks) => {
+      if(item.id == task.id) {
+        tasks.splice(index,1);
+      }
+    });
 
+    window.localStorage.setItem('tasks', JSON.stringify(tasks));
+        renderList();
+      }
+    });
 
-
-
-  li.append(removeButton);
-  li.append(doneButton);
+  li.append(buttons);
+  buttons.append(doneButton);
+  buttons.append(removeButton);
   list.append(li);
 }
 
+function renderList() {
+  const list = $(".tasks");
+  list.html(null);
 
-$('#add-task').click(function() {
+  tasks.forEach(function(item){
+    addTaskToList(item);
+  });
+}
+    
+$('#add-task').click(function(){
   let text = $('#task').val();
+
   $('#task').val(null);
 
-  if (!text) {
+  if(!text) {
     alert('enter fckn text');
-    return;
+      return;
   }
 
   const task = new Task(uuidv4(), text, 'in-progress');
+  tasks.push(task);
 
-  tasks.push(task); 
   addTaskToList(task);
+
   window.localStorage.setItem('tasks', JSON.stringify(tasks));
 });
 
 renderList();
-
-
-
-
-
